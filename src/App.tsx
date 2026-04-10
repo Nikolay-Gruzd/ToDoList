0abe32d059ac98be0637bdef665ea8ef61a72ba4
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-04-10 19:23:43 +0800

    I created the ToDoListItem component and added typing and props to it.
    I added arrays to the App and sent them using props and added the map array method to ToDoListItem to draw them.
    
    I also added a ternary operator to ToDoListItem to check the array for elements so that the corresponding messages can be drawn if they are missing.
    
    Created a Button component with props to use it anywhere with any specified title

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import './App.css'
+import {TodoListItem} from "./components/TodoListItem/TodoListItem.tsx";
 
-function App() {
-  return (
-      <div className="app">
-        <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </div>
-      </div>
-  )
+export type Tasks = {
+    id: number
+    title: string
+    isDone: boolean
 }
 
-export default App
+export const App = () => {
+
+    // Data //
+
+    const title1 = "What to learn"
+    const title2 = "Songs"
+    const title3 = "Books"
+
+    const tasks1: Tasks[] = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "React", isDone: false },
+        { id: 4, title: "Redux", isDone: false },
+        { id: 5, title: "Typescript", isDone: false },
+        { id: 6, title: "RTK query", isDone: false },
+    ]
+    const tasks2: Tasks[] = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am happy", isDone: true },
+        { id: 3, title: "Yo", isDone: false },
+    ]
+    const tasks3: Tasks[] = []
+
+    // Code //
+
+    return (
+        <div className="app">
+            <TodoListItem title={title1} tasks={tasks1} date={"10.04.2026"}/>
+            <TodoListItem title={title2} tasks={tasks2}/>
+            <TodoListItem title={title3} tasks={tasks3}/>
+        </div>
+    )
+}
---
+++ src/components/TodoListItem/TodoListItem.tsx
@@ -0,0 +1,40 @@
+import {Tasks} from "../../App.tsx";
+import {Button} from "../button/Button.tsx";
+
+type Props = {
+    title: string
+    tasks: Tasks[]
+    date?: string
+}
+
+export const TodoListItem = ({title, tasks, date}: Props) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input/>
+                <Button title="+"/>
+            </div>
+            {tasks.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <ul>
+                    {tasks.map(task => {
+                        return (
+                            <li key={task.id}>
+                                <input type="checkbox" checked={task.isDone}/> <span>{task.title}</span>
+                            </li>
+                        )
+                    })}
+                </ul>
+            )
+            }
+            <div>
+                <Button title="All"/>
+                <Button title="Active"/>
+                <Button title="Completed"/>
+            </div>
+            <div>{date}</div>
+        </div>
+    )
+}
