63df33fb2831dd077eefc1168e27c9f87bab2adc
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-04-28 20:55:04 +0800

    Made changes to the code for deleting a job: Added visual deletion for the local state using DeleteTask(). I used the useState hook to redraw the page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {TodoListItem} from "./components/TodoListItem/TodoListItem.tsx";
+import {useState} from "react";
 
 export type Tasks = {
     id: number
@@ -12,31 +13,38 @@ export const App = () => {
     // Data //
 
     const title1 = "What to learn"
-    const title2 = "Songs"
-    const title3 = "Books"
+    // const title2 = "Songs"
+    // const title3 = "Books"
 
-    const tasks1: Tasks[] = [
+    const [tasks1, setTasks1] = useState<Tasks[]> ([
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "React", isDone: false },
         { id: 4, title: "Redux", isDone: false },
         { id: 5, title: "Typescript", isDone: false },
         { id: 6, title: "RTK query", isDone: false },
-    ]
-    const tasks2: Tasks[] = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am happy", isDone: true },
-        { id: 3, title: "Yo", isDone: false },
-    ]
-    const tasks3: Tasks[] = []
+    ])
+    // let tasks2: Tasks[] = [
+    //     { id: 1, title: "Hello world", isDone: true },
+    //     { id: 2, title: "I am happy", isDone: true },
+    //     { id: 3, title: "Yo", isDone: false },
+    // ]
+    // let tasks3: Tasks[] = []
 
     // Code //
 
+    const deleteTask = (taskId: number) => {
+        const filteredTasks = tasks1.filter(task => {
+            return task.id !== taskId
+        })
+        setTasks1(filteredTasks)
+    }
+
     return (
         <div className="app">
-            <TodoListItem title={title1} tasks={tasks1} date={"10.04.2026"}/>
-            <TodoListItem title={title2} tasks={tasks2}/>
-            <TodoListItem title={title3} tasks={tasks3}/>
+            <TodoListItem deleteTask={deleteTask} title={title1} tasks={tasks1} date={"10.04.2026"}/>
+            {/*<TodoListItem deleteTask={deleteTask} title={title2} tasks={tasks2}/>*/}
+            {/*<TodoListItem deleteTask={deleteTask} title={title3} tasks={tasks3}/>*/}
         </div>
     )
 }
