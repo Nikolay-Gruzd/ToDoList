001545dd551411daee5795b3ae99dec4ec141b51
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-05-02 14:18:30 +0800

    Added filtering for Tasks using the useState hook. Added a new type of FilterValues values. To determine the selected type of filtering, I used an IF check for the selected value. And I added a changeFilter for rendering new data, which I sent via props to the TodoListItem component, where it will be called.

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ export type Tasks = {
     isDone: boolean
 }
 
+export type FilterValues = "all" | "active" | "completed"
+
 export const App = () => {
 
     // Data //
 
     const title1 = "What to learn"
-    // const title2 = "Songs"
-    // const title3 = "Books"
 
-    const [tasks1, setTasks1] = useState<Tasks[]> ([
+    const [tasks, setTasks] = useState<Tasks[]> ([
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "React", isDone: false },
@@ -24,27 +24,33 @@ export const App = () => {
         { id: 5, title: "Typescript", isDone: false },
         { id: 6, title: "RTK query", isDone: false },
     ])
-    // let tasks2: Tasks[] = [
-    //     { id: 1, title: "Hello world", isDone: true },
-    //     { id: 2, title: "I am happy", isDone: true },
-    //     { id: 3, title: "Yo", isDone: false },
-    // ]
-    // let tasks3: Tasks[] = []
-
-    // Code //
 
     const deleteTask = (taskId: number) => {
-        const filteredTasks = tasks1.filter(task => {
+        const filteredTasks = tasks.filter(task => {
             return task.id !== taskId
         })
-        setTasks1(filteredTasks)
+        setTasks(filteredTasks)
+    }
+
+    const [filter, setFilter] = useState<FilterValues>("all")
+    let filteredTasks = tasks
+    if (filter === "active") {
+        filteredTasks = tasks.filter( task => !task.isDone)
+    }
+    if ( filter === "completed") {
+        filteredTasks = tasks.filter( task => task.isDone)
+    }
+    const changeFilter = (filter: FilterValues) => {
+        setFilter(filter)
     }
 
     return (
         <div className="app">
-            <TodoListItem deleteTask={deleteTask} title={title1} tasks={tasks1} date={"10.04.2026"}/>
-            {/*<TodoListItem deleteTask={deleteTask} title={title2} tasks={tasks2}/>*/}
-            {/*<TodoListItem deleteTask={deleteTask} title={title3} tasks={tasks3}/>*/}
+            <TodoListItem deleteTask={deleteTask}
+                          title={title1}
+                          tasks={filteredTasks}
+                          changeFilter={changeFilter}
+                          date={"10.04.2026"}/>
         </div>
     )
 }
