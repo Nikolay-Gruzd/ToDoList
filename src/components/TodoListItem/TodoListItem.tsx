bf437418574a4f1675dd1874d212885a92b940a3
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-05-05 15:17:59 +0800

    Added the "Handle" functions, which were used to change the state and re-render the page. I also added a task by pressing "Enter" using "createTaskOnEnterHandler", which checks pressing the correct button.

--- src/components/TodoListItem/TodoListItem.tsx
+++ src/components/TodoListItem/TodoListItem.tsx
@@ -1,32 +1,64 @@
 import {FilterValues, Tasks} from "../../App.tsx";
 import {Button} from "../button/Button.tsx";
+import {ChangeEvent, KeyboardEvent, useState} from "react";
 
 type Props = {
     title: string
     tasks: Tasks[]
     date?: string
-    deleteTask: (taskId: number) => void
+    deleteTask: (taskId: string) => void
+    createTask: (taskTitle: string) => void
     changeToDoListFilter: (filter: FilterValues) => void
 }
 
-export const TodoListItem = ({title, tasks, date, deleteTask, changeToDoListFilter}: Props) => {
+export const TodoListItem = ({
+                                 title,
+                                 tasks,
+                                 date,
+                                 deleteTask,
+                                 createTask,
+                                 changeToDoListFilter
+                             }: Props) => {
+
+    const [taskTitle, setTaskTitle] = useState('')
+
+    const createTaskHandler = () => {
+        createTask(taskTitle)
+        setTaskTitle('')
+    }
+
+    const changeTaskTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTaskTitle(e.currentTarget.value)
+    }
+
+    const createTaskOnEnterHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        e.key === 'Enter' ? createTaskHandler() : null
+    }
+
     return (
         <div>
             <h3>{title}</h3>
             <div>
-                <input/>
-                <Button title="+"/>
+                <input value={taskTitle}
+                       onChange={changeTaskTitleHandler}
+                       onKeyDown={createTaskOnEnterHandler}
+                />
+                <Button title="+" onClick={createTaskHandler}/>
             </div>
             {tasks.length === 0 ? (
                 <p>Тасок нет</p>
             ) : (
                 <ul>
                     {tasks.map(task => {
+                        const deleteTaskHandler = () => {
+                            deleteTask(task.id)
+                        }
+
                         return (
                             <li key={task.id}>
                                 <input type="checkbox" checked={task.isDone}/>
                                 <span>{task.title}</span>
-                                <Button title="x" onClick={() => deleteTask(task.id)}/>
+                                <Button title="x" onClick={deleteTaskHandler}/>
                             </li>
                         )
                     })}
